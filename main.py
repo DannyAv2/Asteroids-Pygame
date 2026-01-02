c8a8ca2a766f7b51757597b8e72d092793cfb7d4
DannyAv2 <dsilerio02@gmail.com>
2026-01-02 00:02:25 -0600

    updated main.py to include the log_state fucntion in the game loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import Player
+from logger import log_state
 def main():
     print("Starting Asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}') 
@@ -11,6 +12,7 @@ def main():
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
