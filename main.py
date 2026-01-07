61b300947a077ea62bacdca2f330896ab22cd83c
DannyAv2 <dsilerio02@gmail.com>
2026-01-07 00:26:06 -0600

    imported the Asteroid/AsteroidField class, using groups and an additional asteroid_field object to wire them to the program.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from constants import *
 from player import Player
 from logger import log_state
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
     print("Starting Asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}') 
@@ -12,9 +15,13 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
-    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
     while True:
@@ -31,3 +38,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
