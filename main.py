e6d5010d786ce52be413077987a580efd76befa3
DannyAv2 <dsilerio02@gmail.com>
2026-01-09 00:26:33 -0600

    imported the shot class, adding its resepcted groups and containers

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from logger import log_state
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from logger import log_event
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
@@ -18,10 +19,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable) 
 
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
